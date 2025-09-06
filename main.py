873f1de04eaecc58145d624d6265bcbf08218a2d
bence <eidzokap@gmail.com>
2025-09-06 02:37:15 +0200

    px

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 import logging
 from dotenv import load_dotenv
 import os
+import random
 
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
@@ -20,11 +21,19 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
 
+def px(s):
+    n = len(s)
+    n = n + random.randint(-3, 5)
+    if n>50:
+        n = 50 + random.randint(-5, 5)
+    return n
+
+
 @client.event
 async def on_message(message):
     if message.author.bot:
         return
-    await message.channel.send(message.content)
+    await message.channel.send(px(message.content))
 
 
 
