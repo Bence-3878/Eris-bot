9cb67e04ffe951d2c99d901a79e23eb59b1cc063
bence <eidzokap@gmail.com>
2025-09-08 22:35:12 +0200

    fix2

--- main.py
+++ main.py
@@ -43,12 +43,9 @@ for i in range(1,100):
 
 ######################init##########################
 
-@client.event
-async def on_ready():
-    print(client.user.name)
-    print(client.user.id)
-    print(leveldb)
-    print(discord.__version__)
+admin_id = 543856425131180036
+
+######################init##########################
 
 def gPX(s):
     n = len(s)
@@ -74,106 +71,7 @@ async def on_message(message):
     if message.author.bot:
         return
 
-    if message.content.startswith('?help'):
-        pass
 
-
-    elif message.content.startswith('?level'):
-        if leveldb is None:
-            await message.channel.send('Az adatbázis nem érhető el, a szint funkció ideiglenesen nem működik.')
-            return
-        cursor = leveldb.cursor()
-        try:
-            cursor.execute(
-                'SELECT id, user_xp, level FROM server_users WHERE id = %s AND server_id = %s',
-                (message.author.id, message.guild.id)
-            )
-            result = cursor.fetchone()
-        finally:
-            cursor.close()
-        if result is None:
-            await message.channel.send('Még nincs adatod ebben a szerverben.')
-            return
-        await message.channel.send('szintje: ' + str(result[2]) +'\n'
-                                   + 'ennyi xp kell a következő szinthez: '
-                                   + str(result[1]-levels[result[2]]) + '/' + str(levels[result[2]+1]-levels[result[2]])
-                                   + '\nösszes xp: ' + str(result[1]))
-
-    elif message.content.startswith('?top'):
-        if leveldb is None:
-            await message.channel.send('Az adatbázis nem érhető el, a toplista ideiglenesen nem működik.')
-            return
-        cursor = leveldb.cursor()
-        try:
-            cursor.execute(
-                'SELECT id, user_xp FROM server_users WHERE server_id = %s ORDER BY user_xp DESC LIMIT 10',
-                (message.guild.id,)
-            )
-            result = cursor.fetchall()
-        finally:
-            cursor.close()
-        embed = discord.Embed(
-            title="top lista",
-            description="a legtöbb üzenet küldő emberek listája",
-            color=discord.Color.blue()
-        )
-
-        for row in result:
-            embed.add_field(
-                name='<@' + str(row[0]) + '>',
-                value=row[1],
-                inline=False
-            )
-
-        await message.channel.send(embed=embed)
-
-    elif message.content.startswith('?test'):
-        await message.channel.send('test')
-
-
-    else:
-        if leveldb is None:
-            return
-        xp = gPX(message.content)
-        cursor = leveldb.cursor()
-        try:
-            cursor.execute(
-                'SELECT user_xp, level FROM server_users WHERE id = %s AND server_id = %s',
-                (message.author.id, message.guild.id)
-            )
-            row = cursor.fetchone()
-            if row is None:
-                try:
-                    cursor.execute(
-                        'INSERT INTO server_users (id, user_xp, level, server_id) VALUES (%s, %s, %s, %s)',
-                        (message.author.id, xp, 0, message.guild.id)
-                    )
-                    leveldb.commit()
-                except mysql.connector.Error as e:
-                    leveldb.rollback()
-                    await message.channel.send(f'Hiba az adatbázis beszúráskor: {e.msg}')
-            else:
-                current_xp = row[0] + xp
-                new_level = level(current_xp)
-                try:
-                    if row[1] < new_level:
-                        channel = client.get_channel(1414239240195149875)
-                        if channel is not None:
-                            await channel.send(f"{message.author.mention} {new_level}. szintű lett")
-                        try:
-                            await message.author.send(f"{new_level}. szintű lettél")
-                        except Exception:
-                            pass
-                    cursor.execute(
-                        'UPDATE server_users SET user_xp = %s, level = %s WHERE id = %s AND server_id = %s',
-                        (current_xp, new_level, message.author.id, message.guild.id)
-                    )
-                    leveldb.commit()
-                except mysql.connector.Error as e:
-                    leveldb.rollback()
-                    await message.channel.send(f'Hiba frissítés közben: {e.msg}')
-        finally:
-            cursor.close()
 
 
 # Esemény: a bot bekerül egy új szerverre
@@ -203,6 +101,12 @@ async def on_guild_join(guild: discord.Guild):
             cursor.close()
 
 
+@client.event  # Eseménykezelő regisztráció
+async def on_member_join(member):  # Akkor fut, amikor új tag csatlakozik egy szerverhez
+    channel = client.get_channel(1411685718740303872)  # Üdvözlő csatorna lekérése ID alapján
+    if channel is not None:  # Ha csatorna létezik és elérhető
+        await channel.send(f"Üdvözöllek {member.mention} ezen a szerveren!")  # Üdvözlő üzenet az új tagnak
+# ... existing code ...  # Helykitöltő
 
 @client.event
 async def on_member_join(member):
