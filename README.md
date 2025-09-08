e535285d663816a463876e5a0ae228471c6a6f3a
bence <eidzokap@gmail.com>
2025-09-08 21:50:39 +0200

    new db

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Eris Bot
 
+[bot](https://discord.com/oauth2/authorize?client_id=1412928677980930108&permissions=8&integration_type=0&scope=bot)
+
--- main.py
+++ main.py
@@ -13,7 +13,7 @@ leveldb = mysql.connector.connect(
     host="localhost",
     user="root",
     password="alma",
-    database="levels",
+    database="discord_bot",
     port=3306,
     auth_plugin='mysql_native_password'
 )
@@ -69,17 +69,27 @@ async def on_message(message):
 
     if message.content.startswith('?help'):
         pass
+
+
     elif message.content.startswith('?level'):
         cursor = leveldb.cursor()
-        cursor.execute('SELECT * FROM users WHERE id = ' + str(message.author.id,))
+        cursor.execute(
+            'SELECT * FROM server_users WHERE id = %s AND server_id = %s',
+            (message.author.id, message.guild.id)
+        )
         result = cursor.fetchone()
+        if result is None:
+            await message.channel.send('Még nincs adatod ebben a szerverben.')
+            return
         await message.channel.send('szintje: ' + str(result[2]) +'\n'
                                    + 'ennyi xp kell a következő szinthez: '
                                    + str(result[1]-levels[result[2]]) + '/' + str(levels[result[2]+1]-levels[result[2]])
                                    + '\nösszes xp: ' + str(result[1]))
+
+
     elif message.content.startswith('?top'):
         cursor = leveldb.cursor()
-        cursor.execute('SELECT * FROM users ORDER BY user_xp DESC LIMIT 10;')
+        cursor.execute('SELECT * FROM server_users ORDER BY user_xp DESC LIMIT 10;')
         result = cursor.fetchall()
         embed = discord.Embed(
             title="top lista",
@@ -99,25 +109,65 @@ async def on_message(message):
 
     elif message.content.startswith('?test'):
         await message.channel.send('test')
+
+
     else:
         xp = gPX(message.content)
         cursor = leveldb.cursor()
-        cursor.execute('SELECT user_xp, level FROM users WHERE id = ' + str(message.author.id,))
+        cursor.execute(
+            'SELECT user_xp, level FROM server_users WHERE id = %s AND server_id = %s',
+            (message.author.id, message.guild.id)
+        )
         result = cursor.fetchall()
+        await message.channel.send(str(len(result)))
         if (len(result) == 0):
-            cursor.execute('INSERT INTO users VALUES (' + str(message.author.id) + ',' + str(xp) + ',0)')
-            leveldb.commit()
+            await message.channel.send('a')
+            try:
+                cursor.execute(
+                    'INSERT INTO server_users (id, user_xp, level, server_id) VALUES (%s, %s, %s, %s)',
+                    (message.author.id, xp, 0, message.guild.id)
+                )
+                leveldb.commit()
+                await message.channel.send('z')
+            except mysql.connector.Error as e:
+                leveldb.rollback()
+                await message.channel.send(f'Hiba az adatbázis beszúráskor: {e.msg}')
         elif (len(result) == 1):
+            await message.channel.send('b')
             currenXP = result[0][0] + xp
-            if result[0][1] < level(currenXP):
-                channel = client.get_channel(1414239240195149875)
-                await channel.send(f"{message.author.mention}  {level(currenXP)}.szintű lett")
-                await message.author.send(str(level(currenXP)) + '.szintű lett')
-            cursor.execute(
-                'UPDATE users SET user_xp = ' + str(currenXP) + ',level = ' +
-            str(level(currenXP)) + ' WHERE id = ' + str(message.author.id, ))
-            leveldb.commit()
+            try:
+                if result[0][1] < level(currenXP):
+                    channel = client.get_channel(1414239240195149875)
+                    await channel.send(f"{message.author.mention}  {level(currenXP)}.szintű lett")
+                    await message.author.send(str(level(currenXP)) + '.szintű lett')
+                cursor.execute(
+                    'UPDATE server_users SET user_xp = %s, level = %s WHERE id = %s AND server_id = %s',
+                    (currenXP, level(currenXP), message.author.id, message.guild.id)
+                )
+                leveldb.commit()
+            except mysql.connector.Error as e:
+                leveldb.rollback()
+                await message.channel.send(f'Hiba frissítés közben: {e.msg}')
 
+# Esemény: a bot bekerül egy új szerverre
+@client.event
+async def on_guild_join(guild: discord.Guild):
+    # Itt futtasd a saját "parancsod" (setup/inicializálás) logikát
+    # Példa: üzenet küldése egy alkalmas csatornába
+    channel = guild.system_channel
+    if channel is None:
+        for ch in guild.text_channels:
+            if ch.permissions_for(guild.me).send_messages:
+                channel = ch
+                break
+
+    if channel is not None:
+        await channel.send("Szia! Köszönöm a meghívást. Készen állok a használatra. Írd be: ?help")
+
+    cursor = leveldb.cursor()
+    cursor.execute('INSERT INTO servers (id, welcome_ch, level_up_ch, level_sys) '
+                   'VALUES ('+str(guild.id)+', NULL, NULL, false)')
+    leveldb.commit()
 
 
 
@@ -126,8 +176,6 @@ async def on_message(message):
 async def on_member_join(member):
     channel = client.get_channel(1411685718740303872)
     await channel.send(f"Üdvözöllek {member.mention} ezen a szerveren!")
-
-
 
 
 client.run(token, log_handler=handler, log_level=logging.DEBUG)
