fa822966ad6ec9e1d74dde42b9705c0e0e085c7f
bence <eidzokap@gmail.com>
2025-09-09 02:47:38 +0200

    üdvüzlő üzenet

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ import os
 import random
 import math
 
-######################import##########################
+
+#####################################################import#############################################################
+
 
 try:
     import mysql.connector
@@ -45,12 +47,8 @@ admin_id = 543856425131180036
 
 ######################init##########################
 
-@client.event
-async def on_ready():
-    print(client.user.name)
-    print(client.user.id)
-    print(leveldb)
-    print(discord.__version__)
+#####################################################init###############################################################
+
 
 def gPX(s):
     n = len(s) + random.randint(-3, 5)
@@ -69,69 +67,72 @@ def level(xp):
         l = i
     return l
 
+###############################################egyszerű függvények######################################################
+
 @client.event
 async def on_message(message):
     if message.author.bot:
         return
-    else:                                           # Minden más üzenet esetén XP kezelés
-        if leveldb is None:                         # Ha nincs DB, nem számolunk XP-t
+    else:
+        if leveldb is None:
             return
-        xp = gPX(message.content)                   # XP becslés az üzenet tartalmából
-        cursor = leveldb.cursor()                   # Kurzor nyitása
+        xp = gPX(message.content)
+        cursor = leveldb.cursor()
 
-        try:                                        # Adatbázis műveletek védett része
+        try:
             cursor.execute('select * from servers where id = %s', (message.guild.id,))
             row1 = cursor.fetchone()
-            cursor.execute(                         # Meglévő adatok lekérdezése
+            cursor.execute(
                 'SELECT user_xp, level FROM server_users WHERE id = %s AND server_id = %s',
                 (message.author.id, message.guild.id)
             )
-            row = cursor.fetchone()                 # Eredmény beolvasása
-            if row is None:                         # Ha új felhasználó ezen a szerveren
-                try:                                # Beszúrás próbálkozás
-                    cursor.execute(                 # Új rekord létrehozása kezdő értékekkel
+            row = cursor.fetchone()
+            if row is None:
+                try:
+                    cursor.execute(
                         'INSERT INTO server_users (id, server_id, user_xp, level) VALUES (%s, %s, %s, %s)',
                         (message.author.id, message.guild.id, xp, 0)
                     )
-                    leveldb.commit()                # Tranzakció véglegesítése
-                except mysql.connector.Error as e:  # DB hiba esetén
-                    leveldb.rollback()              # Visszagörgetés
-                    await message.channel.send(f'Hiba az adatbázis beszúráskor: {e.msg}')  # Hibaüzenet
-            else:                                   # Ha már létezik rekord
-                current_xp = row[0] + xp            # Új összesített XP kiszámítása
-                new_level = level(current_xp)       # Új szint meghatározása
-                try:                                # Frissítés és szintlépés kezelése
-                    if row[1] < new_level:          # Ha szintet lépett a felhasználó
-                        channel = client.get_channel(row1[2])  # Kijelölt szintlépés csatorna lekérése (ID alapján)
-                        if channel is not None:     # Ha a csatorna elérhető
-                            await channel.send(f"{message.author.mention} {new_level}. szintű lett")  # Publikus gratuláció
+                    leveldb.commit()
+                except mysql.connector.Error as e:
+                    leveldb.rollback()
+                    await message.channel.send(f'Hiba az adatbázis beszúráskor: {e.msg}')
+            else:
+                current_xp = row[0] + xp
+                new_level = level(current_xp)
+                try:
+                    if row[1] < new_level:
+                        channel = client.get_channel(row1[2])
+                        if channel is not None:
+                            await channel.send(f"{message.author.mention} {new_level}. szintű lett")
                         if row1[3] == 1:
-                            try:                        # Privát üzenet küldése
-                                await message.author.send(f"{new_level}. szintű lettél")  # DM értesítés
-                            except Exception:           # Ha nem sikerül DM-et küldeni (pl. tiltva)
-                                pass                    # Néma elnyelés
-                    cursor.execute(                 # Felhasználó rekordjának frissítése
+                            try:
+                                await message.author.send(f"{new_level}. szintű lettél")
+                            except Exception:
+                                pass
+                    cursor.execute(
                         'UPDATE server_users SET user_xp = %s, level = %s WHERE id = %s AND server_id = %s',
                         (current_xp, new_level, message.author.id, message.guild.id)
                     )
-                    leveldb.commit()                # Tranzakció véglegesítése
-                except mysql.connector.Error as e:  # Frissítési hiba esetén
-                    leveldb.rollback()              # Visszagörgetés
-                    await message.channel.send(f'Hiba frissítés közben: {e.msg}')  # Hibaüzenet
-        finally:                                    # Mindig lefut
-            cursor.close()                          # Kurzor lezárása
+                    leveldb.commit()
+                except mysql.connector.Error as e:
+                    leveldb.rollback()
+                    await message.channel.send(f'Hiba frissítés közben: {e.msg}')
+        finally:
+            cursor.close()
 
-@client.event
-async def on_guild_join(guild: discord.Guild):
-   channel = guild.system_channel
-    if channel is None:
-        for ch in guild.text_channels:
-            if ch.permissions_for(guild.me).send_messages:
-                channel = ch
-                break
 
-    if channel is not None:
-        await channel.send("Szia! Köszönöm a meghívást. Készen állok a használatra. Írd be: ?help")
+# Esemény: a bot bekerül egy új szerverre  # Kommentezett szekciócím
+@client.event                                       # Eseménykezelő regisztráció
+async def on_guild_join(guild: discord.Guild):      # Akkor fut, amikor a bot egy szerverhez csatlakozik
+    # Itt futtasd a saját "parancsod" (setup/inicializálás) logikát  # Útmutató komment
+    # Példa: üzenet küldése egy alkalmas csatornába  # Példa leírás
+    channel = guild.system_channel                  # Alap csatorna lekérése (ha van)
+    if channel is None:                             # Ha nincs rendszer csatorna
+        for ch in guild.text_channels:              # Végigmegyünk a szöveges csatornákon
+            if ch.permissions_for(guild.me).send_messages:  # Ha a bot írhat ide
+                channel = ch                        # Ezt választjuk csatornának
+                break                               # Megállunk az első megfelelőnél
 
     if leveldb is not None:
         cursor = leveldb.cursor()
@@ -174,9 +175,4 @@ async def on_member_join(member):
         await channel.send(f"Üdvözöllek {member.mention} ezen a szerveren!")
 
 
-if not token:
-    raise RuntimeError("DISCORD_TOKEN nincs beállítva a környezetben (.env).")
-
-
-
-client.run(token, log_handler=handler, log_level=logging.DEBUG)
+client.run(token, log_handler=handler, log_level=logging.DEBUG)  # Bot futtatása a megadott tokennel és naplózási beállításokkal
