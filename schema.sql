ae08f4c90e5b8933d993d97680f49ce1db25632a
bence <eidzokap@gmail.com>
2025-09-28 21:17:35 +0200

    adatbázis séma frissités

--- schema.sql
+++ schema.sql
@@ -13,7 +13,7 @@ CREATE TABLE `servers` (
   `id` bigint UNSIGNED NOT NULL,
   `welcome_ch` bigint UNSIGNED DEFAULT NULL,
   `level_up_ch` bigint UNSIGNED DEFAULT NULL,
-  `level_sys` tinyint(1) DEFAULT 0,
+  `level_system_enabled` tinyint(1) DEFAULT 0,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -21,8 +21,9 @@ CREATE TABLE `server_users` (
   `id` bigint UNSIGNED  NOT NULL,
   `server_id` bigint UNSIGNED NOT NULL,
   `user_xp` int NOT NULL DEFAULT 0,
+  `user_xp_monthly` int NOT NULL DEFAULT 0,
   `level` tinyint UNSIGNED NOT NULL DEFAULT 0,
-  `level_sys` int DEFAULT 0,
+  `level_sys_number` int DEFAULT 0,
   PRIMARY KEY (`id`,`server_id`),
   KEY `fk_server_id` (`server_id`),
   CONSTRAINT `fk_server_id` FOREIGN KEY (`server_id`) REFERENCES `servers` (`id`)
@@ -32,7 +33,7 @@ CREATE TABLE `server_levels` (
   `server_id` bigint UNSIGNED NOT NULL,
   `level` int UNSIGNED NOT NULL DEFAULT 0,
   `role_id` bigint UNSIGNED NOT NULL,
-  `level_sys` int DEFAULT 0,
+  `level_sys_number` int DEFAULT 0,
   PRIMARY KEY (`server_id`,`level`,`role_id`),
   KEY `fk_server_level` (`server_id`),
   CONSTRAINT `fk_server_level` FOREIGN KEY (`server_id`) REFERENCES `servers` (`id`)
