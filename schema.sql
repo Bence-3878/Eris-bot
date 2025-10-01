48c9c74253517cf21609ba9bad07e6854e3b5183
bence <eidzokap@gmail.com>
2025-10-01 15:33:06 +0200

    fix

--- schema.sql
+++ schema.sql
@@ -7,23 +7,26 @@ SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS `server_levels`;
 DROP TABLE IF EXISTS `server_users`;
 DROP TABLE IF EXISTS `servers`;
+DROP TABLE IF EXISTS `users`;
 SET FOREIGN_KEY_CHECKS = 1;
 
 CREATE TABLE `servers` (
     `id` BIGINT UNSIGNED NOT NULL,
     `welcome_ch` BIGINT UNSIGNED DEFAULT NULL,
-    `farewell_ch` BIGINT UNSIGNED DEFAULT NULL,
+    `welcome_msg` VARCHAR(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `goodbye_ch` BIGINT UNSIGNED DEFAULT NULL,
+    `goodbye_msg` VARCHAR(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
     `youtube_ch` BIGINT UNSIGNED DEFAULT NULL,
     `level_up_ch` BIGINT UNSIGNED DEFAULT NULL,
-    `level_system_enabled` tinyint(1) DEFAULT 0,
+    `level_system_enabled` ENUM('enabled','disabled','monthly') DEFAULT 0,
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE `users` (
     `id` BIGINT UNSIGNED NOT NULL,
-    `mal_profil` VARCHAR(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `mal_profile` VARCHAR(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
 
-    PRIMARY KEY (`id`)
+    PRIMARY KEY (`id`)addles
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE `server_users` (
@@ -31,9 +34,12 @@ CREATE TABLE `server_users` (
     `server_id` BIGINT UNSIGNED NOT NULL,
     `user_xp_text` int NOT NULL DEFAULT 0,
     `user_xp_text_monthly` int NOT NULL DEFAULT 0,
+    `user_xp_text_add` int NOT NULL DEFAULT 0,
     `user_xp_voice` int NOT NULL DEFAULT 0,
     `user_xp_voice_monthly` int NOT NULL DEFAULT 0,
-    `level` tinyint UNSIGNED NOT NULL DEFAULT 0,
+    `level_text` tinyint UNSIGNED NOT NULL DEFAULT 0,
+    `level_voice` tinyint UNSIGNED NOT NULL DEFAULT 0,
+    `level_monthly` tinyint UNSIGNED NOT NULL DEFAULT 0,
     `level_sys_number` int DEFAULT 0,
     PRIMARY KEY (`user_id`,`server_id`),
     KEY `fk_server_id` (`server_id`),
@@ -48,6 +54,7 @@ CREATE TABLE `server_levels` (
     `level` INT UNSIGNED NOT NULL DEFAULT 0,
     `role_id` BIGINT UNSIGNED NOT NULL,
     `level_sys_name` VARCHAR(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `monthly` TINYINT(1) UNSIGNED NOT NULL DEFAULT 0,
     PRIMARY KEY (`level_sys_number`),
     UNIQUE KEY `uq_server_level` (`server_id`, `level`),
     KEY `fk_server_level` (`server_id`),
