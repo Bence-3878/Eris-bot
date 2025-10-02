a71d50c142f582cdfe090bba83550ac98cbb4008
bence <eidzokap@gmail.com>
2025-10-02 22:57:22 +0200

    valami

--- schema.sql
+++ schema.sql
@@ -19,13 +19,24 @@ CREATE TABLE `servers` (
     `youtube_ch` BIGINT UNSIGNED DEFAULT NULL,
     `level_up_ch` BIGINT UNSIGNED DEFAULT NULL,
     `level_system_enabled` ENUM('enabled','disabled','monthly') DEFAULT 0,
+    `valami1` BIGINT UNSIGNED DEFAULT NULL,
+    `valami2` BIGINT UNSIGNED DEFAULT NULL,
+    `valami3` BIGINT UNSIGNED DEFAULT NULL,
+    `valami4` VARCHAR(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `valami5` VARCHAR(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `valami6` VARCHAR(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE `users` (
     `id` BIGINT UNSIGNED NOT NULL,
     `mal_profile` VARCHAR(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-
+    `valami1` BIGINT UNSIGNED DEFAULT NULL,
+    `valami2` BIGINT UNSIGNED DEFAULT NULL,
+    `valami3` BIGINT UNSIGNED DEFAULT NULL,
+    `valami4` VARCHAR(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `valami5` VARCHAR(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `valami6` VARCHAR(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
     PRIMARY KEY (`id`)addles
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -41,6 +52,12 @@ CREATE TABLE `server_users` (
     `level_voice` tinyint UNSIGNED NOT NULL DEFAULT 0,
     `level_monthly` tinyint UNSIGNED NOT NULL DEFAULT 0,
     `level_sys_number` int DEFAULT 0,
+    `valami1` BIGINT UNSIGNED DEFAULT NULL,
+    `valami2` BIGINT UNSIGNED DEFAULT NULL,
+    `valami3` BIGINT UNSIGNED DEFAULT NULL,
+    `valami4` VARCHAR(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `valami5` VARCHAR(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `valami6` VARCHAR(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
     PRIMARY KEY (`user_id`,`server_id`),
     KEY `fk_server_id` (`server_id`),
     KEY `fk_user_id` (`user_id`),
@@ -55,6 +72,12 @@ CREATE TABLE `server_levels` (
     `role_id` BIGINT UNSIGNED NOT NULL,
     `level_sys_name` VARCHAR(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
     `monthly` TINYINT(1) UNSIGNED NOT NULL DEFAULT 0,
+    `valami1` BIGINT UNSIGNED DEFAULT NULL,
+    `valami2` BIGINT UNSIGNED DEFAULT NULL,
+    `valami3` BIGINT UNSIGNED DEFAULT NULL,
+    `valami4` VARCHAR(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `valami5` VARCHAR(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `valami6` VARCHAR(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
     PRIMARY KEY (`level_sys_number`),
     UNIQUE KEY `uq_server_level` (`server_id`, `level`),
     KEY `fk_server_level` (`server_id`),
