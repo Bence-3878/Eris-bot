0cd27a26b8318ec212507a8389ac76421cf3aaf0
bence <eidzokap@gmail.com>
2025-11-08 20:02:02 +0100

    fix

--- commands/info/avater.py
+++ commands/info/avater.py
@@ -15,33 +15,40 @@ def create_avatar_command(client):
     """
 
     @app_commands.command(
-        name="avater"
+        name="avater",
+        description="Show user avatar"
     )
-    @app_commands.describe()  # Lokalizációhoz
-    @app_commands.describe(user="")
+    @app_commands.describe(user="The user whose avatar you want to see")
     @app_commands.allowed_installs(guilds=True, users=True)
     @app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
-    async def avatar_command(interaction: discord.Interaction):
+    async def avatar_command(interaction: discord.Interaction, user: discord.User = None):
         """Show user avatar"""
-        if interaction.user.avatar.url == interaction.user.display_avatar.url:
-            msg = language_manager.get_text(language_manager.get_language_for_context(interaction), "avater", "global"
-                                            ,interaction.user.display_name, interaction.user.avatar.url)
+        if user is None:
+            user = interaction.user
+        if user.avatar.url == user.display_avatar.url:
+            msg = language_manager.get_text(language_manager.get_language_for_context(interaction), "avatar", "global",
+                                            user.display_name, user.avatar.url)
         else:
-            msg = language_manager.get_text(language_manager.get_language_for_context(interaction), "avater", "guild",
-                                            interaction.user.display_name, interaction.user.avatar.url,
-                                            interaction.user.display_avatar.url)
+            msg = language_manager.get_text(language_manager.get_language_for_context(interaction), "avatar", "guild",
+                                            user.display_name, user.avatar.url,
+                                            user.display_avatar.url)
         await interaction.response.send_message(msg)
+    
+    return avatar_command
+
 
 def register_avatar_command(tree, client, guild=None):
-    help_cmd = create_avatar_command(client)# Guild-specifikus description beállítása
+    help_cmd = create_avatar_command(client)
+    
+    # Guild-specifikus description beállítása
+    if guild:
+        # Szerver nyelve alapján description módosítás
+        guild_locale = str(guild.preferred_locale)
 
-    # Szerver nyelve alapján description módosítás
-    guild_locale = str(guild.preferred_locale)
-
-    if guild_locale == "hu":
-        help_cmd.description = language_manager.get_command_description("hu", "help")
-    else:
-        help_cmd.description = language_manager.get_command_description("en", "help")
+        if guild_locale == "hu":
+            help_cmd.description = language_manager.get_command_description("hu", "avatar")
+        else:
+            help_cmd.description = language_manager.get_command_description("en", "avatar")
 
     tree.add_command(help_cmd, guild=guild)
 
